de0c27341dcfb723c9571c9e1ba70c71c922d8bb
Anurag Pramanik <p.anurag@outlook.com>
2022-04-18 21:11:51 +0530

    fix router issue

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,17 @@ import ForgotPassword from './pages/ForgotPassword'
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Explore />} />
-        <Route path='/offers' element={<Offers />} />
-        <Route path='/profile' element={<SignIn />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-      </Routes>
-      <Navbar />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
     </>
   )
 }
