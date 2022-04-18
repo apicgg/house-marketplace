22323baa6d485accc24e0da60f3ff6ef4bb43196
Anurag Pramanik <p.anurag@outlook.com>
2022-04-18 21:07:07 +0530

    add sign in page

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,14 @@ import ForgotPassword from './pages/ForgotPassword'
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Explore />} />
-          <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<SignIn />} />
-          <Route path='/sign-in' element={<SignIn />} />
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route path='/forgot-password' element={<ForgotPassword />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path='/' element={<Explore />} />
+        <Route path='/offers' element={<Offers />} />
+        <Route path='/profile' element={<SignIn />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+      </Routes>
       <Navbar />
     </>
   )
