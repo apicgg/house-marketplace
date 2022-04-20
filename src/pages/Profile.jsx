fd77adeeecc5994d515b0905e22e3e440a06af20
Anurag Pramanik <p.anurag@outlook.com>
2022-04-20 10:32:35 +0530

    add profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,83 @@
+import { useEffect, useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import { getAuth, updateProfile } from 'firebase/auth'
+import { updateDoc } from 'firebase/firestore'
+import { db } from '../firebase.config'
 function Profile() {
+  const auth = getAuth()
+  const navigate = useNavigate()
+
+  const [changeDetails, setChangeDetails] = useState(false)
+
+  const [formData, setFormdata] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  })
+
+  const { name, email } = formData
+
+  const onLogout = () => {
+    auth.signOut()
+
+    navigate('/sign-in')
+  }
+
+  const onSubmit = () => {
+    console.log(123)
+  }
+
+  const onChange = (e) => {
+    setFormdata((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className='profile'>
+      <header className='profileHeader'>
+        <p className='pageHeader'>My Profile</p>
+        <button className='logOut' type='button' onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+
+      <main>
+        <div className='profileDetailsHeader'>
+          <p className='profileDetailsText'>Personal Details</p>
+          <p
+            className='changePersonalDetails'
+            onClick={() => {
+              changeDetails && onSubmit()
+              setChangeDetails((prevState) => !prevState)
+            }}
+          >
+            {changeDetails ? 'done' : 'change'}
+          </p>
+        </div>
+
+        <div className='profileCard'>
+          <form>
+            <input
+              type='text'
+              id='name'
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+
+            <input
+              type='email'
+              id='email'
+              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   )
 }
