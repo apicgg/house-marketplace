72f1c5a7c142a8729aae95fb532fbff8acd56227
Anurag Pramanik <p.anurag@outlook.com>
2022-04-22 11:16:40 +0530

    map feature to add for listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom'
 import { doc, getDoc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.config'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
 
